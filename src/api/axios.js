b80def835ad89464d3a24565a5b3b94f67300213
Claudio Paredes Arbelo <claudioparedesarbelo@gmail.com>
2024-12-30 19:46:16 -0300

    arreglando conectividad con back

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://my-node-app-50fq.onrender.com/api/tasks',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    
     withCredentials: true,
   });
 
