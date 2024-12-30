1fe54d1e874bc5156d9f6eea19df68b313ee4e1c
Claudio Paredes Arbelo <claudioparedesarbelo@gmail.com>
2024-12-30 15:29:35 -0300

    Front cambio enlace

--- src/api/url.js
+++ src/api/url.js
@@ -1,1 +1,5 @@
-export const BASE_URL = 'http://localhost:8080/api/tasks/'
+/* export const BASE_URL = 'http//localhost:8080/api/tasks/' */
+
+export const BASE_URL = 'https//backtask.railway.internal:8080/api/tasks/'
+
+
